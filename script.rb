1491b0c1f0c9885a8afe0822fb93c729c6aa3288
cwylie0 <cwylie0@hotmail.com>
2018-03-14 17:45:59 -0400

    added script.rb

---
+++ script.rb
@@ -0,0 +1,30 @@
+=begin
+These are some examples of basic commands
+in the Ruby programming language
+=end
+
+my_num =  13   # Add your code here!
+
+my_boolean = true    # And here!
+
+my_name = "cwylie0"
+my_age = 41
+
+puts my_num
+puts my_boolean
+puts my_name
+
+puts my_name.length
+puts my_name.reverse
+puts my_name.upcase
+puts my_name.downcase
+
+sum = 13 + 379
+product = 923 * 15
+quotient = 13209 / 17
+
+puts sum
+puts product
+puts quotient
+
+puts my_name.downcase.reverse.upcase
